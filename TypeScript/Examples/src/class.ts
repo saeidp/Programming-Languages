5184f3a63f5cc3967e53f0132c4b504c8326712c
Saeid Pahlevan <saeid.pahlevan@curtin.edu.au>
2024-05-22 07:13:37 +0800

    typescript class

--- TypeScript/Examples/src/class.ts
+++ TypeScript/Examples/src/class.ts
@@ -1,62 +1,117 @@
 export { }
-// --------------------------defining class--------------------
-// initializing a class definition
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
+// // --------------------------defining class--------------------
+// // initializing a class definition
+// class Greeter {
+//     greeting: string;
+//     constructor(message: string) {
+//         this.greeting = message;
+//     }
+//     greet() {
+//         return "Hello, " + this.greeting;
+//     }
+// }
+
+// let greeter = new Greeter("world");
+// console.log(greeter.greet());
+// // -----------------------------------------Inheritance-------------------------
+// class Animal {
+//     move(distanceInMeters: number = 0) {
+//         console.log(`Animal moved ${distanceInMeters}m.`);
+//     }
+// }
+
+// class Dog extends Animal {
+//     bark() {
+//         console.log("Woof! Woof!");
+//     }
+// }
+
+// const dog = new Dog();
+// dog.bark();
+// dog.move(10);
+// dog.bark();
+
+// // ---------------------------------------More classes-----------------
+// class Hero {
+//     name: string;
+//     level: number;
+//     constructor(name: string, level: number) {
+//         this.name = name;
+//         this.level = level;
+//     }
+
+//     // adding a method to the constructor
+//     greet() {
+//         return `${this.name} says hello.`;
+//     }
+// }
+
+// const hero = new Hero("saeid", 5);
+// console.log(hero.greet());
+
+// // creating a new class from the parent
+// class Mage extends Hero {
+//     spell: string;
+//     constructor(name: string, level: number, spell: string) {
+//         // chain constructor with super
+//         super(name, level);
+
+//         // add a new property
+//         this.spell = spell;
+//     }
+// }
+
+//  ---------------------------------------------------- a class to extends another class and implement an interface --------------------
+interface IMemoryData {
+    get(id: string): string;
+    set(id: string, value: string): void;
+}
+
+interface IPersistentData {
+    save(): string;
+    load(state: string): void;
+}
+
+class MemoryData implements IMemoryData {
+    protected data: { [key: string]: string } = {};
+    // or protected data: Record<string, string> = {};
+    get(id: string): string {
+        return this.data[id];
     }
-    greet() {
-        return "Hello, " + this.greeting;
+    set(id: string, value: string): void {
+        this.data[id] = value;
     }
 }
 
-let greeter = new Greeter("world");
-console.log(greeter.greet());
-// -----------------------------------------Inheritance-------------------------
-class Animal {
-    move(distanceInMeters: number = 0) {
-        console.log(`Animal moved ${distanceInMeters}m.`);
+class PersistentData extends MemoryData implements IPersistentData {
+    save(): string {
+        return JSON.stringify(this.data)
+    }
+    load(state: string): void {
+        this.data = JSON.parse(state)
+        // load data
     }
 }
 
-class Dog extends Animal {
-    bark() {
-        console.log("Woof! Woof!");
-    }
-}
+const persistentData = new PersistentData();
 
-const dog = new Dog();
-dog.bark();
-dog.move(10);
-dog.bark();
+persistentData.set("name", "John");
+console.log(persistentData.get("name"));
 
-// ---------------------------------------More classes-----------------
-class Hero {
-    name: string;
-    level: number;
-    constructor(name: string, level: number) {
-        this.name = name;
-        this.level = level;
-    }
+const saved = persistentData.save();
 
-    // adding a method to the constructor
-    greet() {
-        return `${this.name} says hello.`;
-    }
-}
+persistentData.set("name", "Smith");
+console.log(persistentData.get("name"));
 
-const hero = new Hero("saeid", 5);
-console.log(hero.greet());
 
-// creating a new class from the parent
-class Mage extends Hero {
-    spell: string;
-    constructor(name: string, level: number, spell: string) {
-        // chain constructor with super
-        super(name, level);
+const persistentData2 = new PersistentData();
+persistentData2.load(saved);
+console.log(persistentData2.get("name"));
+// or if the data is not protected
+// console.log(persistentData2.data["name"]);
 
-        // add a new property
-        this.spell = spell;
-    }
-}
+persistentData.set("age", "10");
+console.log(persistentData.get("age"));
+
+
+
